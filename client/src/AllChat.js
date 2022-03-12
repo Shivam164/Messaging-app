d2f0f5955e496c00c80914e3b4af8272238719cb
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-12 16:37:27 +0530

    almost done with styling in allchat and message sections

--- client/src/AllChat.js
+++ client/src/AllChat.js
@@ -2,21 +2,31 @@ import React from 'react';
 import './styles/AllChat.css';
 import CallIcon from '@mui/icons-material/Call';
 import CommentIcon from '@mui/icons-material/Comment';
+import Person from './Person';
 
 function AllChat() {
   return (
     <div className='allChat'>
         <div className='allChat__header'>
           <div className='header__text'>
-            <button className='open__btn'>Open</button>
-            <button className='done__btn'>Done</button>
-            <button className='unread__btn'>Unread</button>
+            <button>Open</button>
+            <button>Done</button>
+            <button>Unread</button>
           </div>
           <div className='header__icons'>
                 <CallIcon  style={{ color: "#23e001" }}/>
                 <CommentIcon style={{ color: "#23e001" }}/>
           </div>
         </div>
+
+        {/* All Chat  */}
+        <div className='allPerson'>
+          <Person selected = {false}/>
+          <Person selected = {true}/>
+          <Person selected = {false}/>
+          <Person selected = {false}/>
+        </div>
+        
     </div>
   )
 }
--- client/src/MessagesSection.js
+++ client/src/MessagesSection.js
@@ -1,10 +1,52 @@
 import React from 'react'
 import './styles/MessagesSection.css';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SendIcon from '@mui/icons-material/Send';
+import SingleMsg from './SingleMsg';
+
 
 function MessagesSection() {
   return (
     <div className='msgSection'>
-        <h1>Message Section</h1>
+
+      {/* HEADER  */}
+        <div className="msgSection__header">
+          <div className='header__image'>
+            <img src = "https://userpic.codeforces.org/2018443/title/38fb16c17026a84c.jpg" />
+          </div>
+          <div className="header__content">
+            <div className="header__personInfo">
+              <p>Shivam</p>
+              <small>shivam-ug20@nsut.ac.in</small>
+            </div>
+            <div className='header__icons'>
+              <MoreVertIcon style={{ color: "gray" }} />
+            </div>
+          </div>
+        </div>
+
+        {/* ALL MESSAGES  */}
+        <div className='allMessages'>
+          <SingleMsg own = {false}/>
+          <SingleMsg own = {true} />
+          <SingleMsg own = {true} />
+          <SingleMsg own = {true} />
+          <SingleMsg own = {false}/>
+          <SingleMsg own = {false}/>
+          <SingleMsg own = {false}/>
+          <SingleMsg own = {false}/>
+          <SingleMsg own = {false}/>
+
+        </div>
+
+        {/* MESSAGE INPUT  */}
+        <div className="messageInput">
+          <input/>
+          <button>
+          <SendIcon/>
+          </button>
+        </div>
+
     </div>
   )
 }
