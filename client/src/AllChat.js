7742edd564fdfb2edf33cd2316441a82af17aae2
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-12 09:50:02 +0530

    created header

--- client/src/AllChat.js
+++ client/src/AllChat.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles/AllChat.css';
+import CallIcon from '@mui/icons-material/Call';
+import CommentIcon from '@mui/icons-material/Comment';
 
 function AllChat() {
   return (
     <div className='allChat'>
-        <h1>AllChat</h1>
+        <div className='allChat__header'>
+          <div className='header__text'>
+            <button className='open__btn'>Open</button>
+            <button className='done__btn'>Done</button>
+            <button className='unread__btn'>Unread</button>
+          </div>
+          <div className='header__icons'>
+                <CallIcon  style={{ color: "#23e001" }}/>
+                <CommentIcon style={{ color: "#23e001" }}/>
+          </div>
+        </div>
     </div>
   )
 }
