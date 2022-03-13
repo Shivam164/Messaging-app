6036e042d362d5e571cd5c05e7371a937f6b804f
Ankush Goel <77546629+AnkushGoel251@users.noreply.github.com>
2022-03-13 13:14:47 +0530

    phone_number_signIn

--- client/src/PhoneNumberInfo.js
+++ client/src/PhoneNumberInfo.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import './styles/PhoneNumberInfo.css';
 
-function PhoneNumberInfo() {
+const PhoneNumberInfo = () => {
   return (
-    <div>
-        <h1>PhoneNumberInfo</h1>
+    <div className="wrapper">
+      <h1 className="main-heading">Sign in</h1>
+      <p className="sub-text">Please verify your phone number.</p>
+        <div className="form-wrapper">
+          <form id="loginForm">
+            <div className="input-field">
+              <input
+                type="text"
+                placeholder="Phone Number"
+                name="phone"
+                autoComplete="false"
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="OTP"
+                name="phone"
+                autoComplete="false"
+              />
+            </div>
+            <button className="main-button" type="submit" id="sign-in-button">
+              Sign in
+            </button>
+          </form>
         </div>
-  )
-}
+    </div>
+  );
+};
 
 export default PhoneNumberInfo
