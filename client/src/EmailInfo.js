eed3922847f3749465190c7ae03583404cb56058
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-13 13:16:35 +0530

    added email portion of signIn

--- client/src/EmailInfo.js
+++ client/src/EmailInfo.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './styles/EmailInfo.css';
 
 function EmailInfo() {
   return (
-    <div>
-        <h1>Email Info</h1>
+    <div className='emailInfo'>
+        <h1>Create an account</h1>
+        <input placeholder='Enter Your Name' />
+        <input placeholder='Enter Your Email' />
+        <input placeholder='Enter Your Password' type = 'password'/>
+
+        <button>Sign Up</button>
+        <div className='logIn'>
+            <span>Already have an account?</span>
+            <button>Log in</button>
+        </div>
+
     </div>
   )
 }
