394a7bc51018cd53400acda24ab862adeab2ed74
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-13 14:02:58 +0530

    done with styling signIn portion

--- client/src/PhoneNumberInfo.js
+++ client/src/PhoneNumberInfo.js
@@ -4,30 +4,30 @@ import './styles/PhoneNumberInfo.css';
 const PhoneNumberInfo = () => {
   return (
     <div className="wrapper">
-      <h1 className="main-heading">Sign in</h1>
+      <h1 className="main-heading">Add Your Phone Number</h1>
       <p className="sub-text">Please verify your phone number.</p>
-        <div className="form-wrapper">
-          <form id="loginForm">
-            <div className="input-field">
-              <input
-                type="text"
-                placeholder="Phone Number"
-                name="phone"
-                autoComplete="false"
-              />
-              <br />
-              <input
-                type="text"
-                placeholder="OTP"
-                name="phone"
-                autoComplete="false"
-              />
-            </div>
-            <button className="main-button" type="submit" id="sign-in-button">
-              Sign in
-            </button>
-          </form>
+        <input
+            type="text"
+            placeholder="Phone Number"
+            name="phone"
+            autoComplete="false"
+            />
+
+        <button className="main-button" type="submit" id="sign-in-button">
+            Send OTP
+        </button>
+
+        <div className='OTP__check'>
+           <input
+            type="text"
+            placeholder="OTP"
+            name="phone"
+            autoComplete="false"
+            /> 
+            <button>Verify</button>
         </div>
+            
+        
     </div>
   );
 };
--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -3,13 +3,14 @@ import './styles/SignIn.css';
 import Carousel from 'react-elastic-carousel';
 import EmailInfo from './EmailInfo';
 import PhoneNumberInfo from './PhoneNumberInfo';
+import pic from './assets/signIn-img.png';
 
 function SignIn() {
   return (
     <div className='outerBox'>
         <div className='innerContainer'>
             <div className='progress'>
-
+                <img src = {pic} className='signIn__image'/>
             </div>
             <div className='info'>
                 <Carousel className='slider'>
