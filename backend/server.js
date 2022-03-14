3f96720b88fad47be2a467afc0bb21133d13eb7b
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-14 22:00:27 +0530

    connected with local mongodb databse

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.json());
 
+const URI = 'mongodb://localhost:27017/MessagingApp';
+
+// SETTING UP CONNECTION WITH THE DATABASE
+mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology : true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to the database.");
+    })
+    .catch(err => console.log(err))
+
 app.listen(3000, () => {
     console.log("listening to port 3000");
 })
