b775dfbdc8a12ca23a2b5cef5ac77a6260875a51
Ankush Goel <77546629+AnkushGoel251@users.noreply.github.com>
2022-03-17 12:24:20 +0530

    chat_info_styling

--- client/src/ChatInfo.js
+++ client/src/ChatInfo.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './styles/ChatInfo.css'
+import WorkIcon from '@mui/icons-material/Work';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function ChatInfo() {
   return (
@@ -8,20 +12,16 @@ function ChatInfo() {
         <h2 className='heading'>Shivam</h2><br />
         <div className='Info-flex' align='left'>
            <div>
-           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfeW1siu3VhX3F59CRpR2fOPgJdDCDFZOdTG_quTyjTerf29JqdAIhOpOE61nXX8jiEy0&usqp=CAU" className='icon-img'></img> Company
-               <textarea></textarea>
+           <WorkIcon />Company :  Google 
           </div>
           <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJZ9WCaRNgk94ot5rm4Lo4F9YW5M7Vc7uMWg&usqp=CAU" className='icon-img'></img>  Role
-                 <textarea className='text-role'></textarea>
+            <AccountCircleIcon />Role : SDE 
           </div>
           <div>
-            <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png" className='icon-img'></img> Phone no
-                <textarea></textarea>
+            <PhoneIcon/>Phone no : 493794872
           </div>
           <div>
-            <img src="https://cdn.icon-icons.com/icons2/2768/PNG/512/email_icon_176616.png" className='icon-img'></img>  E-Mail id.
-                <textarea></textarea>
+            <EmailIcon />E-Mail id. : akdjfk@gmail.com
           </div>
         </div>
     </div>
